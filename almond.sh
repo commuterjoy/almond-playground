690faa09a7ef7b51e651ff5795a339e4c2571dd7
Matt Chadburn <matt@commuterjoy.co.uk>
2012-10-05 19:10:28 +0100

    Made almond.sh produce everything

--- almond.sh
+++ almond.sh
@@ -1,3 +1,13 @@
+
+# generate the bootstraps with requirejs
 time r.js -o conf
+
+# wrap almond around the pre-domready bootstrap
 time r.js -o baseUrl=. name=almond.js uglify=none include=tmp/bootstraps/predomready.js out=tmp/bootstraps/predomready.almond.js
+
+# copy article bootstrap to htdocs
+cp -v tmp/bootstraps/article.js public_html/article.js
+
+# embed the pre-domready bootstrap in the index.html
+ruby -e 'puts IO.read("public_html/index.template").gsub(/PREDOMREADYBOOTSTRAP/, IO.read("tmp/bootstraps/predomready.almond.js"))' > public_html/index.html
 
